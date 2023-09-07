be677d3019769c7579e1dad0b2fc35b53d667727
Yida Zhang <zyd200101@outlook.com>
2023-09-07 13:47:13 -0400

    prevent changes of makefile

--- PA1/grading.sh
+++ PA1/grading.sh
@@ -5,7 +5,7 @@ ln -s ../../llvm/ llvm
 ln -s ../../llvm/Debug+Asserts/lib/
 ln -s ../../llvm/Debug+Asserts/bin/
 cd uscc
-make clean
-make
+make -f ../../Makefile clean
+make -f ../../Makefile
 cd tests
 python2 testParse.py
